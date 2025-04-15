38024e2b8da0c672d1fd06d83af6a959dfaa6dd9
Jagdish <jagdishjena93@gmail.com>
2025-04-16 00:15:44 +0530

    update App.jsx with latest changes

--- Accordion/src/App.jsx
+++ Accordion/src/App.jsx
@@ -4,6 +4,13 @@ import "./App.css";
 const App = () => {
   return (
     <div>
+      <h1>Accordion</h1>
+      <p>
+        An accordion is a UI component that allows users to expand and collapse
+        sections of content. It is commonly used to display information in a
+        compact and organized manner. Users can click on the headers of the
+        accordion sections to reveal or hide the content within those sections.
+      </p>
       <Accordion />
     </div>
   );
